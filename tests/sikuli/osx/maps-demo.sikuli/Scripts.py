efc16e5a9c5b45710de97363243046c19471586b
Colin MacDonald <colin@goinstant.com>
2013-10-22 14:50:13 -0300

    remove sikuli and selenium tests

--- tests/sikuli/osx/maps-demo.sikuli/Scripts.py
+++
@@ -1,8 +0,0 @@
-def resizeApp(app, dx, dy):
-2        switchApp(app)
-3        corner = find(Pattern().targetOffset(3,14))
-4
-5        drop_point = corner.getTarget().offset(dx, dy)
-6        dragDrop(corner, drop_point)
-7
-8resizeApp("Safari", 50, 50)
